82527bd96321e8be13901b78534f7223a76fed11
hehong <hehg78@qq.com>
2023-06-14 08:53:13 +0800

    rm tmp file

--- test_swig/build.sh
+++ test_swig/build.sh
@@ -10,10 +10,6 @@ python_libs_dir=`python-config --prefix`/lib
 swig -python my_lib.i
 clang -o _my_lib.so -fpic -shared my_lib.c my_lib_wrap.c $python_libs_include -L$python_libs_dir -lpython
 
-#for callback
-#swig -c++ -python  my_callback_lib.i
-clang++ -o _my_callback_lib.so -std=c++11 -fpic -shared my_cxx_class.cpp my_callback_lib_wrap.cxx $python_libs_include -L$python_libs_dir -lpython
-
 #user python3.9
 
 python_libs_include=/Library/Developer/CommandLineTools/Library/Frameworks/Python3.framework/Headers
--- test_swig/my_callback_lib.i
+++
@@ -1,8 +0,0 @@
-%module(directors="1") my_callback_lib
-%feature("director") my_cxx_class;
-
-%{
-#include "my_cxx_class.h"
-%}
-
-%include "my_cxx_class.h"
